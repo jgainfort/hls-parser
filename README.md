45fe5f7994e78a5ada44a4182cd7e8df7f2e7176
John G. Gainfort, Jr <john@realeyes.com>
2018-12-07 15:56:39 -0700

    updated proj structure to include webpage wrapper, added console log capability

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 A fast and lightweight HTTP Live Streaming (HLS) parsing WebAssembly library written in Rust
 
 POC Stage, DO NOT USE!
+
+## Usage
+
+## Build
+
+```wasm-pack build```
+
+## Test in Headless Browsers with wasm-pack test
+
+```wasm-pack test --headless --firefox```
+
+## Publish to NPM with wasm-pack publish
+
+```wasm-pack publish```
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,55 @@
+extern crate cfg_if;
 extern crate wasm_bindgen;
+extern crate web_sys;
 
+mod utils;
+
+use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}", name));
+extern "C" {}
+
+#[wasm_bindgen]
+pub enum PlaylistType {
+    Master,
+    Media,
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[wasm_bindgen]
+pub enum StreamType {
+    Event,
+    Live,
+    Vod,
+}
+
+#[wasm_bindgen]
+pub struct DescriptiveTag {}
+
+#[wasm_bindgen]
+pub struct Playlist {
+    playlistType: PlaylistType,
+    tags: Vec<DescriptiveTag>,
+}
+
+#[wasm_bindgen]
+pub fn parse(manifest: &str, url: &str) -> Playlist {
+    console::log_2(&"manifest: ".into(), &manifest.into());
+    console::log_2(&"url: ".into(), &url.into());
+
+    Playlist {
+        playlistType: PlaylistType::Master,
+        tags: Vec::new(),
     }
 }
